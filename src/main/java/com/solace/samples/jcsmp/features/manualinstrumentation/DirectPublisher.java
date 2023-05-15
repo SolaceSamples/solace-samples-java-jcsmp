ae8c4b13a56f154537f4da3d28616c58b98d6342
mayur-solace <mayur.patel@solace.com>
2023-05-15 11:04:56 -0400

    Added OpenTelemetry/Tracing HowTo Snippet and feature sample

---
+++ src/main/java/com/solace/samples/jcsmp/features/manualinstrumentation/DirectPublisher.java
@@ -0,0 +1,156 @@
+/*
+ * Copyright 2022-2023 Solace Corporation. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.solace.samples.jcsmp.features.manualinstrumentation;
+
+import com.solace.messaging.trace.propagation.SolaceJCSMPTextMapSetter;
+import com.solacesystems.jcsmp.Destination;
+import com.solacesystems.jcsmp.JCSMPException;
+import com.solacesystems.jcsmp.JCSMPFactory;
+import com.solacesystems.jcsmp.JCSMPProperties;
+import com.solacesystems.jcsmp.JCSMPSession;
+import com.solacesystems.jcsmp.JCSMPStreamingPublishCorrelatingEventHandler;
+import com.solacesystems.jcsmp.TextMessage;
+import com.solacesystems.jcsmp.Topic;
+import com.solacesystems.jcsmp.XMLMessage;
+import com.solacesystems.jcsmp.XMLMessageProducer;
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.SpanKind;
+import io.opentelemetry.api.trace.StatusCode;
+import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.context.Context;
+import io.opentelemetry.context.Scope;
+import io.opentelemetry.context.propagation.TextMapPropagator;
+import io.opentelemetry.semconv.trace.attributes.SemanticAttributes;
+import io.opentelemetry.semconv.trace.attributes.SemanticAttributes.MessagingDestinationKindValues;
+
+/**
+ * A sample that shows how to generate a Publisher/send span with Solace OpenTelemetry Integration
+ * for JCSMP.
+ * <p>
+ * Setup a Solace PubSub+ Broker and OpenTelemetry Collector as per tutorial  >
+ * https://codelabs.solace.dev/codelabs/dt-otel/index.html
+ * <p>
+ * This is the Publisher in the Publish-Subscribe messaging pattern.
+ */
+public class DirectPublisher {
+
+  private static final String SERVICE_NAME = "SolaceJCSMPTopicPublisherManualInstrument";
+  private static final String SAMPLE_NAME = DirectPublisher.class.getSimpleName();
+  private static final String TOPIC_NAME = "solace/tracing";
+  private static final String API = "JCSMP";
+
+  static {
+    //Setup OpenTelemetry
+    TracingUtil.initManualTracing(SERVICE_NAME);
+  }
+
+  public void run(String... args) throws JCSMPException {
+    final JCSMPProperties properties = new JCSMPProperties();
+    properties.setProperty(JCSMPProperties.HOST, args[0]);         // host:port
+    properties.setProperty(JCSMPProperties.VPN_NAME, args[1]);     // message-vpn
+    properties.setProperty(JCSMPProperties.USERNAME, args[2]);     // client-username
+    if (args.length > 3) {
+      properties.setProperty(JCSMPProperties.PASSWORD, args[3]);  // client-password
+    }
+
+    final JCSMPSession session = JCSMPFactory.onlyInstance().createSession(properties);
+    session.connect();  // connect to the broker
+
+    // Simple anonymous inner-class for handling publishing events
+    final XMLMessageProducer producer = session.getMessageProducer(
+        new JCSMPStreamingPublishCorrelatingEventHandler() {
+          // unused in Direct Messaging application, only for Guaranteed/Persistent publishing application
+          @Override
+          public void responseReceivedEx(Object key) {
+          }
+
+          // can be called for ACL violations, connection loss, and Persistent NACKs
+          @Override
+          public void handleErrorEx(Object key, JCSMPException cause, long timestamp) {
+            log("### Producer handleErrorEx() callback: %s%n", cause);
+          }
+        });
+
+    log(API + " " + SAMPLE_NAME + " connected");
+
+    final Topic topic = JCSMPFactory.onlyInstance().createTopic(TOPIC_NAME);
+    final TextMessage message = JCSMPFactory.onlyInstance().createMessage(TextMessage.class);
+    message.setText("Hello World!!");
+
+    final OpenTelemetry openTelemetry = GlobalOpenTelemetry.get();
+    final Tracer tracer = openTelemetry.getTracer(SERVICE_NAME);
+
+    //TRACE MESSAGE PUBLISH
+    messagePublisherTracer(message, producer, topic, openTelemetry, tracer);
+
+    session.closeSession();  // will also close producer object
+  }
+
+  private void messagePublisherTracer(XMLMessage message, XMLMessageProducer messagePublisher,
+      Destination messageDestination, OpenTelemetry openTelemetry, Tracer tracer) {
+
+    final Span sendSpan = tracer
+        .spanBuilder("mySolacePublisherApp > send")
+        .setSpanKind(SpanKind.PRODUCER)
+        //Optional: user defined Span attributes
+        .setAttribute("SERVICE_NAME", SERVICE_NAME)
+        .setAttribute(SemanticAttributes.MESSAGING_SYSTEM, "SolacePubSub+")
+        .setAttribute(SemanticAttributes.MESSAGING_OPERATION, "send")
+        .setAttribute(SemanticAttributes.MESSAGING_DESTINATION, messageDestination.getName())
+        .setAttribute(SemanticAttributes.MESSAGING_DESTINATION_KIND,
+            MessagingDestinationKindValues.TOPIC)
+        .setAttribute(SemanticAttributes.MESSAGING_TEMP_DESTINATION, false)
+        .setParent(Context.current()) // set current context as parent
+        .startSpan();
+
+    try (Scope scope = sendSpan.makeCurrent()) {
+      final SolaceJCSMPTextMapSetter setter = new SolaceJCSMPTextMapSetter();
+      final TextMapPropagator propagator = openTelemetry.getPropagators().getTextMapPropagator();
+      //and then inject current context with send span into the message
+      propagator.inject(Context.current(), message, setter);
+      //message is being published to the given destination
+      messagePublisher.send(message, messageDestination);
+    } catch (Exception e) {
+      sendSpan.recordException(e); //Span can record exception if any
+      sendSpan.setStatus(StatusCode.ERROR, e.getMessage());
+    } finally {
+      sendSpan.end(); //Span data is exported when span.end() is called
+    }
+  }
+
+  public static void main(String... args) throws JCSMPException {
+    if (args.length < 3) {  // Check command line arguments
+      log("Usage: %s <host:port> <message-vpn> <client-username> [password]%n%n", SAMPLE_NAME);
+      System.exit(-1);
+    }
+    log(API + " " + SAMPLE_NAME + " initializing...");
+
+    new DirectPublisher().run(args);
+
+    log("Main thread quitting.");
+  }
+
+  private static void log(String logMsg) {
+    System.out.println(logMsg);
+  }
+
+  private static void log(String logMsg, Object... args) {
+    System.out.println(String.format(logMsg, args));
+  }
+}
---
+++ src/main/java/com/solace/samples/jcsmp/snippets/HowToImplementManualInstrumentation.java
@@ -0,0 +1,172 @@
+/*
+ * Copyright 2022-2023 Solace Corporation. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.solace.samples.jcsmp.snippets;
+
+import com.solace.messaging.trace.propagation.SolaceJCSMPTextMapGetter;
+import com.solace.messaging.trace.propagation.SolaceJCSMPTextMapSetter;
+import com.solacesystems.jcsmp.Topic;
+import com.solacesystems.jcsmp.XMLMessage;
+import com.solacesystems.jcsmp.XMLMessageProducer;
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.SpanKind;
+import io.opentelemetry.api.trace.StatusCode;
+import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.context.Context;
+import io.opentelemetry.context.Scope;
+import io.opentelemetry.context.propagation.TextMapPropagator;
+import io.opentelemetry.semconv.trace.attributes.SemanticAttributes;
+import io.opentelemetry.semconv.trace.attributes.SemanticAttributes.MessagingDestinationKindValues;
+import io.opentelemetry.semconv.trace.attributes.SemanticAttributes.MessagingOperationValues;
+import java.util.function.Consumer;
+
+public class HowToImplementManualInstrumentation {
+
+  /**
+   * Example how to inject a tracing context into Solace Message before it is published to a queue
+   * or topic.
+   *
+   * @param messageToPublish A Solace JCSMP message to be used for publishing.
+   * @param openTelemetry    The entry point to telemetry functionality for tracing, metrics and
+   *                         baggage.
+   */
+  void howToInjectTraceContextInSolaceMessage(XMLMessage messageToPublish,
+      OpenTelemetry openTelemetry) {
+    final SolaceJCSMPTextMapSetter setter = new SolaceJCSMPTextMapSetter();
+    // Injects current context into the message to transport it across message boundaries.
+    // Transported context will be used to create parent - child relationship
+    // between spans from different services and broker spans
+    final Context contextToInject = Context.current();
+    openTelemetry.getPropagators().getTextMapPropagator()
+        .inject(contextToInject, messageToPublish, setter);
+  }
+
+  /**
+   * Example how to extract a tracing context from the JCSMP Solace Message upon message receipt.
+   *
+   * @param receivedMessage Received Solace message.
+   * @param openTelemetry   The entry point to telemetry functionality for tracing, metrics and
+   *                        baggage.
+   */
+  void howToExtractTraceContextIfAnyFromSolaceMessage(XMLMessage receivedMessage,
+      OpenTelemetry openTelemetry) {
+    //Extracts tracing context from a message, if any using the SolaceJCSMPTextMapGetter
+    final SolaceJCSMPTextMapGetter getter = new SolaceJCSMPTextMapGetter();
+    final Context extractedContext = openTelemetry.getPropagators().getTextMapPropagator()
+        .extract(Context.current(), receivedMessage, getter);
+    //and then set the extractedContext as current context
+    try (Scope scope = extractedContext.makeCurrent()) {
+      //...
+    }
+  }
+
+  /**
+   * Example how to inject a tracing context in the Solace Message and generate a SEND span for the
+   * published message
+   *
+   * @param message            A Solace message that support tracing context propagation.
+   * @param messageProducer    JCSMP Message producer that can publish messages
+   * @param messageDestination message will be published to this topic
+   * @param openTelemetry      The entry-point to telemetry functionality for tracing, metrics and
+   *                           baggage.
+   * @param tracer             Tracer is the interface for Span creation and interaction with the
+   *                           in-process context.
+   */
+  void howToCreateSpanOnMessagePublish(XMLMessage message, XMLMessageProducer messageProducer,
+      Topic messageDestination, OpenTelemetry openTelemetry, Tracer tracer) {
+
+    //Create a new span with a current context as parent of this span
+    final Span sendSpan = tracer
+        .spanBuilder("mySolacePublisherApp" + " " + MessagingOperationValues.PROCESS)
+        .setSpanKind(SpanKind.CLIENT)
+        // published to a topic endpoint (non temporary)
+        .setAttribute(SemanticAttributes.MESSAGING_DESTINATION_KIND,
+            MessagingDestinationKindValues.TOPIC)
+        .setAttribute(SemanticAttributes.MESSAGING_TEMP_DESTINATION, false)
+        //Set more attributes as needed
+        //.setAttribute(...)
+        //.setAttribute(...)
+        .setParent(Context.current()) // set current context as parent
+        .startSpan();
+
+    //set sendSpan as new current context
+    try (Scope scope = sendSpan.makeCurrent()) {
+      final SolaceJCSMPTextMapSetter setter = new SolaceJCSMPTextMapSetter();
+      final TextMapPropagator propagator = openTelemetry.getPropagators().getTextMapPropagator();
+      //and then inject current context with send span into the message
+      propagator.inject(Context.current(), message, setter);
+      // message is being published to the given topic
+      messageProducer.send(message, messageDestination);
+    } catch (Exception e) {
+      sendSpan.recordException(e); //Span can record exception if any
+      sendSpan.setStatus(StatusCode.ERROR, e.getMessage()); //Set span status as ERROR/FAILED
+    } finally {
+      sendSpan.end(); //End sendSpan. Span data is exported when span.end() is called.
+    }
+  }
+
+  /**
+   * Example how to extract a tracing context from the Solace Message and generate a RECEIVE span
+   * for the received message
+   *
+   * @param receivedMessage  A Solace message.
+   * @param messageProcessor A callback function that user could use to process a message
+   * @param openTelemetry    The OpenTelemetry class is the entry point to telemetry functionality
+   *                         for tracing, metrics and baggage from OpenTelemetry Java SDK.
+   * @param tracer           OpenTelemetry Tracer is the interface from OpenTelemetry Java SDK for
+   *                         span creation and interaction with the in-process context.
+   */
+  void howToCreateNewSpanOnMessageReceive(XMLMessage receivedMessage,
+      Consumer<XMLMessage> messageProcessor,
+      OpenTelemetry openTelemetry, Tracer tracer) {
+
+    //Extract tracing context from message, if any using the SolaceJCSMPTextMapGetter
+    final SolaceJCSMPTextMapGetter getter = new SolaceJCSMPTextMapGetter();
+    final Context extractedContext = openTelemetry.getPropagators().getTextMapPropagator()
+        .extract(Context.current(), receivedMessage, getter);
+
+    //Set the extracted context as current context
+    try (Scope scope = extractedContext.makeCurrent()) {
+      //Create a child span and set extracted/current context as parent of this span
+      final Span receiveSpan = tracer
+          .spanBuilder("mySolaceReceiverApp" + " " + MessagingOperationValues.RECEIVE)
+          .setSpanKind(SpanKind.CLIENT)
+          // for the case the message was received on a non temporary queue endpoint
+          .setAttribute(SemanticAttributes.MESSAGING_DESTINATION_KIND,
+              MessagingDestinationKindValues.QUEUE)
+          .setAttribute(SemanticAttributes.MESSAGING_TEMP_DESTINATION, false)
+          //Set more attributes as needed
+          //.setAttribute(...)
+          //.setAttribute(...)
+          // creates a parent child relationship to a message publisher's application span is any
+          .setParent(extractedContext)
+          // starts span
+          .startSpan();
+
+      try {
+        // do something with a message in a callback
+        messageProcessor.accept(receivedMessage);
+      } catch (Exception e) {
+        receiveSpan.recordException(e); //Span can record exception if any
+        receiveSpan.setStatus(StatusCode.ERROR,
+            e.getMessage()); //and set span status as ERROR/FAILED
+      } finally {
+        receiveSpan.end(); //End receiveSpan. Span data is exported when span.end() is called.
+      }
+    }
+  }
+}
