b578b4be884c2a6249db6158823455fd1a1abaa8
aaron-613 <github@aaronlee.ca>
2024-08-02 18:40:04 -0700

    removed toString() causing NPE, fixed indenting

--- src/main/java/com/solace/samples/jcsmp/features/distributedtracing/CustomTextMapGetter.java
+++ src/main/java/com/solace/samples/jcsmp/features/distributedtracing/CustomTextMapGetter.java
@@ -8,40 +8,31 @@ import java.util.Map;
 
 
 public class CustomTextMapGetter implements TextMapGetter<Object> {
-	
+
 	private static final List<String> FIELDS = Collections.unmodifiableList(
-		      Arrays.asList(new String[] { "tracestate", "traceparent", "baggage" }));
-		  
-	  public Iterable<String> keys(Object carrier) {
-	    return FIELDS;
-	  }
-	  
-	  @SuppressWarnings("unchecked")
+			Arrays.asList(new String[] { "tracestate", "traceparent", "baggage" }));
+
+	public Iterable<String> keys(Object carrier) {
+		return FIELDS;
+	}
+
+	@SuppressWarnings("unchecked")
 	public String get(Object map, String key) {
-		  if (null == key || key.isEmpty() || null == map) {
-			  return null;
-		  }
-		  String traceId = ((Map<String, String>)map).get("otel_parent_trace_id").toString();		    		
-		  String spanId = ((Map<String, String>)map).get("otel_parent_span_id").toString();
+		if (null == key || key.isEmpty() || null == map) {
+			return null;
+		}
+		String traceId = ((Map<String, String>)map).get("otel_parent_trace_id");		    		
+		String spanId = ((Map<String, String>)map).get("otel_parent_span_id");
 
-		  if ("tracestate".equals(key))
-			  return null;
-		  if ("traceparent".equals(key))
-		  {
-
-			  if (traceId == null || spanId == null)
-			  {
-				  return null;	
-			  } 
-			  else 
-			  {
-				  return "00-" + traceId + "-" + spanId + "-01";
-			  }
-		  }
-		  if ("baggage".equals(key))
-			  return null;
-		  
+		if ("tracestate".equals(key))  return null;
+		if ("traceparent".equals(key)) {
+			if (traceId == null || spanId == null) {
+				return null;	
+			} else {
+				return "00-" + traceId + "-" + spanId + "-01";
+			}
+		}
+		if ("baggage".equals(key)) return null;
 		return "";
-
-	  }
+	}
 }
