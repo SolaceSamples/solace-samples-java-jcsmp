d55d71793574a27b516ad29b4404eb6f5805db8f
Shaan Chander <shaan.chander@solace.com>
2026-09-21 11:50:52 -0400

    EBP-3100: Use the official OpenTelemetry for Solace JCSMP API name in the publisher Javadoc

--- src/main/java/com/solace/samples/jcsmp/features/distributedtracing/GuaranteedPublisherWithManualInstrumentation.java
+++ src/main/java/com/solace/samples/jcsmp/features/distributedtracing/GuaranteedPublisherWithManualInstrumentation.java
@@ -45,8 +45,8 @@ import io.opentelemetry.context.propagation.TextMapPropagator;
 import io.opentelemetry.semconv.SemanticAttributes;
 
 /**
- * A sample that shows how to generate a Publisher/send span with Solace OpenTelemetry Integration
- * for JCSMP.
+ * A sample that shows how to generate a Publisher/send span with OpenTelemetry for Solace JCSMP
+ * API.
  * <p>
  * Setup a Solace Event Broker and OpenTelemetry Collector as per tutorial  >
  * https://codelabs.solace.dev/codelabs/dt-otel/index.html
